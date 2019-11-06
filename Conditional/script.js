14cc71800f179cd14035c75f7d25a1c2459115bc
Shankara Narayanan <M1054012@mindtree.com>
2019-11-06 17:16:45 +0530

    Initial Commit for basic conditionals

---
+++ Conditional/script.js
@@ -0,0 +1,17 @@
+// // let name = '';
+// if (name) {
+// 	alert('The Name is a string');
+// } else if (typeof name === 'number') {
+// 	alert('The Name is a number');
+// } else {
+// 	alert('The Name is not a string');
+// }
+let name = 10;
+// console.log(name, typeof name);x
+name
+	? typeof name === 'string'
+		? alert('THE NAME IS A STRING')
+		: typeof name === 'number'
+		? alert('THE NAME IS A NUMBER')
+		: alert('THE NAME IS EVIL')
+	: alert('THERE IS NO NAME');
