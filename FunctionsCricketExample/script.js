1f8a4ee2e19f666e1ec53bb9aa9fb3aebbc3bea1
Shankara Narayanan <M1054012@mindtree.com>
2019-11-12 15:23:16 +0530

    Added Over counter

--- FunctionsCricketExample/script.js
+++ FunctionsCricketExample/script.js
@@ -1,6 +1,45 @@
 let score = 0;
+let wicket = 0;
+let balls = 0;
+let overs = 0;
+
+function updateDOM(target, value) {
+	document.getElementById(target).innerHTML = value;
+}
+
+function handleBalls() {
+	balls++;
+	updateDOM('balls', balls);
+}
+
+function updateOvers() {
+	overs++;
+	updateDOM('overs', overs);
+}
+
 function addScore(value) {
-	console.log('Value Received => ', value);
 	score += value;
-	document.getElementById('score').innerHTML = score;
+	if (balls >= 6) {
+		updateOvers();
+		balls = 0;
+		updateDOM('balls', balls);
+		return;
+	}
+	handleBalls();
+	updateDOM('score', score);
+	updateDOM('balls', balls);
+	updateDOM('overs', overs);
+}
+
+function addWicket() {
+	if (wicket >= 10) {
+		alert('GAME OVER !!');
+		return;
+	}
+	wicket = wicket + 1;
+	updateDOM('wicket', wicket);
+}
+
+function calculateOvers() {
+	updateDOM('overs', 1);
 }
